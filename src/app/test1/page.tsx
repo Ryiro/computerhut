dc323ae943da8da8a3293e71e8d78769bdd83dc4
Ryiro <redgearryiro@gmail.com>
2024-11-08 10:44:17 +0530

    testing multi functionality

---
+++ src/app/test1/page.tsx
@@ -0,0 +1,4 @@
+const test1 = () => {
+  return <div>This is test1 page</div>;
+};
+export default test1;
---
+++ src/app/test2/page.tsx
@@ -0,0 +1,4 @@
+const test2 = () => {
+  return <div>This is test2 page</div>;
+};
+export default test2;
